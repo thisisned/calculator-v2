7f6712b9a35cc70297c0325dff0d75b59437410e
Ned <ned.wilson.eames@gmail.com>
2023-02-05 17:42:32 +0000

    Fixed floating point precision bug thing

--- script.js
+++ script.js
@@ -17,22 +17,24 @@ const divide = function (a, b) {
 };
 
 function operate(op, a, b) {
+    let ans;
     switch (op) {
         case "add":
-            return add(a, b);
+            ans = add(a, b);
             break;
         case "subtract":
-            return subtract(a, b);
+            ans = subtract(a, b);
             break;
         case "divide":
-            return divide(a, b);
+            ans = divide(a, b);
             break;
         case "multiply":
-            return multiply(a, b);
+            ans = multiply(a, b);
             break;
         default:
             break;
     }
+    return Number((ans).toFixed(12));
 };
 
 let newNum = true;
@@ -41,12 +43,16 @@ let operandTwo = 0;
 let displayValue = 0;
 let operator = "";
 
+const numberButtons = document.querySelectorAll('.num-button')
+const operatorButtons = document.querySelectorAll('.op-button')
 const display = document.querySelector("#calc-display");
+const equalButton = document.querySelector('#eq');
 
 function updateDisplay(value) {
     if (newNum) {
         displayValue = value;
         newNum = false;
+        operatorButtons.forEach((button) => { button.classList.remove('op-active') });
     }
     else {
         displayValue += value;
@@ -54,15 +60,11 @@ function updateDisplay(value) {
     display.innerHTML = displayValue;
 }
 
-const numberButtons = document.querySelectorAll('.num-button')
-
 numberButtons.forEach(function (currentBtn) {
     currentBtn.addEventListener('click', function () {
         updateDisplay(currentBtn.id);
     });
 })
-
-const operatorButtons = document.querySelectorAll('.op-button')
 
 operatorButtons.forEach(function (currentBtn) {
     currentBtn.addEventListener('click', function () {
@@ -73,11 +75,10 @@ operatorButtons.forEach(function (currentBtn) {
     });
 })
 
-const equalButton = document.querySelector('#eq');
-
 equalButton.addEventListener('click', function () {
     operandTwo = parseInt(display.innerHTML);
     let answer = operate(operator, operandOne, operandTwo);
     display.innerHTML = answer;
     operatorButtons.forEach((button) => { button.classList.remove('op-active') });
+    newNum = true;
 })
